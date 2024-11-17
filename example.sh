bd5b48037abd4cc5727e7d0b393a8948aced51ab
Kush, Nishchal <kush@kush.com.fj>
2024-11-17 20:14:36 +0930

    fixed virtualenv path

--- example.sh
+++ example.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ./python_env/bin/activate
+source ./python_venv/bin/activate
 
 python ./test_harness.py -s server -n 10 -l ids_slaves_list.txt > run_ids_servers.sh
 python ./test_harness.py -s client -n 3 -l ids_slaves_list.txt > run_ids_clients.sh
